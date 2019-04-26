bb22abae26c4eb92d8b50b4b3fc45ccb4aa39063
Arthur BINOIS <abinois@e2r6p13.42.fr>
2019-04-26 20:16:10 +0200

    la libft de arthur binois

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: abinois <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 17:39:52 by abinois           #+#    #+#             */
-/*   Updated: 2019/04/12 12:02:03 by abinois          ###   ########.fr       */
+/*   Updated: 2019/04/26 20:15:11 by abinois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,7 @@ char	*ft_strchr(const char *s, int c)
 {
 	if (c < 0)
 		return (NULL);
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char*)s);
+	while (*s && *s != c)
 		s++;
-	}
-	if (*s == (char)c)
-		return ((char*)s);
-	return (NULL);
+	return (*s == c ? (char*)s : NULL);
 }
